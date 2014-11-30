efe8e0db3f3f2c9903e20d3d7f3c572e769049ad
Ed <edgrams333@gmail.com>
2014-11-29 22:53:26 -0600

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Ed on 11/29/14.
+ */
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json')
+    });
+
+    grunt.registerTask('default',[]);
+};

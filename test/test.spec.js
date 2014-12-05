46db70e676b9bcf5aab7a7a0c03484f485464561
Ed Grams <edgramswork@gmail.com>
2014-12-04 20:32:56 -0600

    TSBEX-3 #time 1h jasmine / requirejs / bower hookup

---
+++ test/test.spec.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Ed on 12/4/14.
+ */
+
+define(['test'], function(m) {
+    describe('test', function() {
+        it('should just work', function() {
+            expect(true).toBeTruthy();
+        })
+    });
+});

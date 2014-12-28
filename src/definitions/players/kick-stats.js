393c974172afde8cace64e7adfe60a6b5d4fda11
Ed Grams <edgramswork@gmail.com>
2014-12-28 00:57:10 -0600

    TSBEX-7 #time 1h kick punt stats

---
+++ src/definitions/players/kick-stats.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Ed on 12/28/14.
+ */
+
+define(function() {
+
+    function Definition() {
+
+        return {
+            fieldGoalAttempts: undefined,
+            fieldGoalsMade: undefined,
+            extraPointAttempts: undefined,
+            extraPointsMade: undefined
+        }
+    }
+
+    return Definition;
+});

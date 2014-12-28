eaf56f8e265953238a45bc886a80eccb3a07c141
Ed Grams <edgramswork@gmail.com>
2014-12-28 00:27:49 -0600

    TSBEX-7 #time 1h def stats

---
+++ src/definitions/players/def-player-stats.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Ed on 12/28/14.
+ */
+
+define(function() {
+
+    function Definition() {
+
+        return {
+            sacks: undefined,
+            interceptions: undefined,
+            intYards: undefined,
+            intTouchdowns: undefined
+        }
+    }
+
+    return Definition;
+});
